99a9ed86fe71e6e46c2e34181da55e2918668b63
Alexis Lejeune <alexis.lejeune@gmx.fr>
2019-09-03 20:35:47 +0000

    First step

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Entity\Vote;
+use App\Form\VoteConferenceType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/conference", name="conference")
+     * @Route("/conference/{id}", name="conference_vote")
      */
-    public function index()
+    public function getConference(Conference $conference, Request $request, EntityManagerInterface $em)
     {
+        $user = $this->getUser();
+        $vote = new Vote();
+        $form = $this->createForm(VoteConferenceType::class,$vote);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $vote->setUser($user);
+            $vote->setConference($conference);
+            $user->addVote($vote);
+            $conference->addVote($vote);
+            $em->persist($vote);
+            $em->flush();
+            return $this->redirectToRoute('conference_vote',['id' => $conference->getId()]);
+        }
+
+
         return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
+            'form' => $form->createView(),
+            'conference' => $conference
         ]);
     }
 }
--- src/Form/VoteConferenceType.php
+++ src/Form/VoteConferenceType.php
@@ -4,7 +4,7 @@ namespace App\Form;
 
 use App\Entity\Vote;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,12 +14,17 @@ class VoteConferenceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('value',RangeType::class, [
-                'attr' => [
-                    'min' => 0,
-                    'max' => 5
+            ->add('value',ChoiceType::class, [
+                'choices' => [
+                    '0' => 0,
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5
                 ],
-                'required' => false
+                'expanded' => true,
+                'multiple' => false
             ])
             ->add('submit', SubmitType::class)
         ;
