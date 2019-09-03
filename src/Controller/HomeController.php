4edc991d5e676e8dc086aa718979fe8534d7209d
Alexis Lejeune <alexis.lejeune@gmx.fr>
2019-09-03 14:34:54 +0000

    Pagination for conferences

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/home", name="home")
-     */
-    public function index()
+    private $conferenceRepository;
+    private $paginator;
+
+    public function __construct(ConferenceRepository $conferenceRepository, PaginatorInterface $paginator)
     {
+        $this->conferenceRepository = $conferenceRepository;
+        $this->paginator = $paginator;
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function getConferences(PaginatorInterface $paginator, Request $request) // 3 by 3
+    {
+        //$conferences = $this->conferenceRepository->findBy([], [],3,$offset);
+        $conferences = $this->conferenceRepository->findAll();
+        //$nbOfPages = count($allConferences) / 3;
+        //dd($nbOfPages);
+        //$paginator  = $this->get('knp_paginator');
+        $pagination = $this->paginator->paginate(
+            $conferences, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            3 /*limit per page*/
+        );
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'conferences' => $pagination
         ]);
     }
 }
