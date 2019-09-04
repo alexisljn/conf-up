46a5cf8659b2a9a56a869aae2d22e4fc56bf9526
Alexis Lejeune <alexis.lejeune@gmx.fr>
2019-09-04 12:51:58 +0000

    CRUD on Conference

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,85 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Form\CreateConferenceType;
+use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin/conference", name="admin_conferences")
      */
-    public function index()
+    public function getConferences(ConferenceRepository $conferenceRepository)
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        $conferences = $conferenceRepository->findAll();
+
+        return $this->render('admin/conferences.html.twig', [
+            'conferences' => $conferences,
         ]);
+    }
+
+    /**
+     * @Route("/admin/conference/{id}", name="admin_conference")
+     */
+    public function getConference(Conference $conference)
+    {
+        return $this->render('admin/conference.html.twig', [
+            'conference' => $conference
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/create/conference", name="create_conference")
+     */
+    public function createConference(Request $request, EntityManagerInterface $em)
+    {
+        $conference = new Conference();
+        $form = $this->createForm(CreateConferenceType::class, $conference);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->persist($conference);
+            $em->flush();
+            return $this->redirectToRoute('admin_conference',['id' => $conference->getId()]);
+        }
+
+        return $this->render('admin/create-conf.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/edit/conference/{id}", name="edit_conference")
+     */
+    public function editConference(Conference $conference, EntityManagerInterface $em, Request $request)
+    {
+        $form = $this->createForm(CreateConferenceType::class, $conference);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('admin_conference',['id' => $conference->getId()]);
+        }
+
+        return $this->render('admin/edit-conf.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/delete/conference/{id}", name="delete_conference")
+     */
+    public function deleteConference(Conference $conference, EntityManagerInterface $em)
+    {
+        $em->remove($conference);
+        $em->flush();
+
+        return $this->redirectToRoute('admin_conferences');
     }
 }
